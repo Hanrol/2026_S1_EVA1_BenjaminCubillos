3bf80d6364c3a0176d882a5ab0922b5ac5f64da2
Hanrol <be.cubillosp@duocuc.cl>
2026-03-31 10:26:07 -0300

    primer commit

---
+++ src/main/java/cl/duoc/benjamincubillos/BenjamincubillosApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.benjamincubillos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BenjamincubillosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BenjamincubillosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/duoc/benjamincubillos/BenjamincubillosApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.benjamincubillos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BenjamincubillosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
