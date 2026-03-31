0776546ef6d96dc4407dd344455d29fb8acf33a2
Hanrol <be.cubillosp@duocuc.cl>
2026-03-31 11:00:24 -0300

    Service listo

---
+++ src/main/java/cl/duoc/benjamincubillos/service/SolicitudService.java
@@ -0,0 +1,48 @@
+package cl.duoc.benjamincubillos.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import cl.duoc.benjamincubillos.model.Solicitud;
+import cl.duoc.benjamincubillos.repository.SolicitudRepository;
+
+@Service
+public class SolicitudService {
+
+    @Autowired
+    private SolicitudRepository solicitudRepository;
+
+    public List<Solicitud> getSolicitudes() {
+        return solicitudRepository.listarSolicitudes();
+    }
+
+    public String saveSolicitud(Solicitud sol) {
+        if (solicitudRepository.buscarPorId(sol.getId()).isPresent()) {
+            return "ID_DUPLICADO";
+        }
+
+        solicitudRepository.agregarSolicitud(sol);
+        return "OK";
+    }
+
+    public String updateSolicitudId(Solicitud sol) {
+        if (solicitudRepository.buscarPorId(sol.getId()).isPresent()) {
+            solicitudRepository.actuaSolicitud(sol);
+            return "OK";
+        } else {
+            return "NOT_FOUND";
+        }
+    }
+
+    public String deleteSolicitudId(Long id) {
+        solicitudRepository.eliminarSolicitudId(id);
+        return "Producto eliminado.";
+    }
+
+    public Optional<Solicitud> getSolicitudId(Long id) {
+        return solicitudRepository.buscarPorId(id);
+    }
+}
